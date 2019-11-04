47ae57aa56377e0b3627993ab5bde06a45b16307
Yamin LEGZOULI <ylegzoul@e3r4p1.42.fr>
2019-11-04 15:39:24 +0100

    all fuction is

--- libft.h
+++ libft.h
@@ -47,13 +47,13 @@ void				ft_putnbr_fd(int nb, int fd);
 
 //------------------------------------ IS ---------------------------------------\\
 
-//int				ft_isalpha(int c);
-//int				ft_isdigit(int c);
-//int				ft_isalnum(int c);
-//int				ft_isascii(int c);
-//int				ft_isprint(int c);
-//int				ft_toupper(int c);
-//int				ft_tolower(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
 
 //---------------------------------- AUTRES -------------------------------------\\
 
