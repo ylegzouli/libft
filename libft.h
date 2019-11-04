6521699913350d3958cbec55f312e741cdc90b88
Yamin LEGZOULI <ylegzoul@e3r4p1.42.fr>
2019-11-04 15:15:03 +0100

    putstr

--- libft.h
+++ libft.h
@@ -37,7 +37,7 @@ char				**ft_split(char const *s, char c);
 //------------------------------------ PUT --------------------------------------\\
 
 void				ft_putchar(char c);
-//void				ft_putstr(char const *s);
+void				ft_putstr(char const *s);
 //void				ft_putendl(char const *s);
 void				ft_putnbr(int nb);
 void				ft_putchar_fd(char c, int fd);
